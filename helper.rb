dc5758785613bfcf0df235f8722a78be49f131d4
Sho Hashimoto <hashimoto@shokai.org>
2011-02-09 05:36:37 +0900

    login twitter with oauth

--- helper.rb
+++ helper.rb
@@ -9,10 +9,32 @@ require 'kconv'
 require 'oauth'
 require 'twitter'
 
+set :sessions, true
+
 begin
   @@conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml').read
   p @@conf
 rescue => e
   STDERR.puts 'config.yaml load error!'
   STDERR.puts e
+end
+
+def app_root
+  "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
+end
+
+def consumer
+  OAuth::Consumer.new(@@conf['twitter_key'], @@conf['twitter_secret'],
+                      :site => "http://twitter.com")
+end
+
+def auth_uri
+  @request_token = consumer.get_request_token(:oauth_callback => "#{app_root}/auth")
+  session[:request_token] = @request_token.token
+  session[:request_token_secret] = @request_token.secret
+  @request_token.authorize_url
+end
+
+def remove_twitter_cmd(str)
+  str.gsub(/^d/i,'')
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,50 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-def app_root
-  "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
+def auth?
+  return true if session[:access_token] and session[:access_token_secret]
+  return false
 end
 
 get '/' do
   haml :index
 end
 
+get '/auth' do
+  @request_token = OAuth::RequestToken.new(consumer,
+                                           session[:request_token],
+                                           session[:request_token_secret])
+  @access_token = @request_token.get_access_token({},
+                                                  :oauth_token => params[:oauth_token],
+                                                  :oauth_verifier => params[:oauth_verifier])
+  session[:access_token] = @access_token.token
+  session[:access_token_secret] = @access_token.secret
+  redirect app_root
+end
+
+get '/login' do
+  redirect auth_uri
+end
+
+get '/logout' do
+  session[:access_token] = nil
+  session[:access_token_secret] = nil
+  redirect app_root
+end
+
+post '/tweet' do
+  message = params['message']
+  if !message or message.to_s.size < 1
+    @mes = {:error => 'message is null'}.to_json
+  else
+    Twitter.configure do |config|
+      config.consumer_key = @@conf['twitter_key']
+      config.consumer_secret = @@conf['twitter_secret']
+      config.oauth_token = session[:access_token]
+      config.oauth_token_secret = session[:access_token_secret]
+    end
+    res = Twitter.update remove_twitter_cmd(message)
+    @mes = {:message => message, :response => res}.to_json
+  end
+  
+end
